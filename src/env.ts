577059d4cdf3a6af6b8b7a69b7292ec7c9a7ae2d
Javed Ansari <javedans2003@gmail.com>
2024-08-23 22:17:40 +0530

    enviroments setup

--- src/env.ts
+++ src/env.ts
@@ -6,9 +6,18 @@ export const env = createEnv({
   extends: [vercel()],
   server: {
     DATABASE_URL: z.string().url(),
+    PUSHER_APP_ID: z.string(),
+    PUSHER_SECRET_KEY: z.string(),
   },
-  client: {},
+  client: {
+    NEXT_PUBLIC_PUSHER_CLUSTER: z.string(),
+    NEXT_PUBLIC_PUSHER_KEY: z.string(),
+  },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    PUSHER_APP_ID: process.env.PUSHER_APP_ID,
+    NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
+    PUSHER_SECRET_KEY: process.env.PUSHER_SECRET_KEY,
+    NEXT_PUBLIC_PUSHER_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_CLUSTER
   },
 });
