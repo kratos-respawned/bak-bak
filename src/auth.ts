de843f89b3222cff768957e74eee848e8e1730f8
Gaurav Bhandari <sbhandari1907@protonmail.com>
2024-08-23 22:13:38 +0530

    setup next-auth

---
+++ src/auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+ 
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./lib/prisma"
+ 
+
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+})
--- src/env.ts
+++ src/env.ts
@@ -6,9 +6,15 @@ export const env = createEnv({
   extends: [vercel()],
   server: {
     DATABASE_URL: z.string().url(),
+    AUTH_SECRET: z.string(),
+    AUTH_GITHUB_ID: z.string(),
+    AUTH_GITHUB_SECRET: z.string(),
   },
   client: {},
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
+    AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
   },
 });
