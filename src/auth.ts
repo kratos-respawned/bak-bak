3c6e9114a92a3a9d90729acbfcabda6c1a2a1055
Gaurav Bhandari <sbhandari1907@protonmail.com>
2024-08-24 00:28:57 +0530

    add chat schemas

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,14 @@
 import NextAuth from "next-auth"
-import authConfig from "./auth.config"
  
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import GitHub from "next-auth/providers/github"
 import { prisma } from "./lib/prisma"
  
 
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: { strategy: "jwt" },
-  ...authConfig,
+  session: { strategy: "database" },
+  providers: [GitHub]
+  // ...authConfig,
 })
